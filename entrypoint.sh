4a37ed32733fe373acc0f3712555ce80f3646dbc
David Lonjon <david.lonjon@gmail.com>
2020-09-10 22:07:12 +0900

    Pass credential to svn checkout command

--- entrypoint.sh
+++ entrypoint.sh
@@ -55,7 +55,7 @@ SVN_DIR="/github/svn-${SLUG}"
 # Checkout just trunk and assets for efficiency
 # Tagging will be handled on the SVN level
 echo "➤ Checking out .org repository..."
-svn checkout --depth immediates "$SVN_URL" "$SVN_DIR"
+svn checkout --depth immediates --non-interactive  --username "$SVN_USERNAME" --password "$SVN_PASSWORD" "$SVN_URL" "$SVN_DIR"
 cd "$SVN_DIR"
 svn update --set-depth infinity assets
 svn update --set-depth infinity trunk
